a3598ff871811006de48a9c9b53e2b883ce642be
hhiruko <adilet.toktybay@gmail.com>
2025-07-30 09:47:50 +0500

    fix: no args

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,30 @@ mod services;
 
 use std::env;
 
+fn version() {
+    println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    for (i, arg) in args.iter().enumerate().skip(1) {
-        println!("Arg {}: {}", i, arg);
+    #[cfg(debug_assertions)]
+    {
+        for (i, arg) in args.iter().enumerate().skip(1) {
+            println!("Arg {}: {}", i, arg);
+        }
+    }
+
+    if args.len() == 1 {
+        version();
+        return;
     }
 
     let vars: Vec<String> = (&args[2..]).to_vec();
 
     match args[1].as_str() {
         "password" => services::password::resolve(vars),
-        _          => println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+        "version" | "v" => version(),
+        _          => {}
     }
 }
